330523e6ecd080b2087b5ab03d2fedccb2e1a881
Syed Ali <sali3@cox.net>
2016-12-12 16:36:19 -0500

    added directive to add transclude data

---
+++ app/panel.directive.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular.module('gh')
+    .directive('panel', PanelDirective);
+
+
+
+  function PanelDirective() {
+
+    return{
+      templateUrl:'/app/panel.template.html',
+      restrict: 'A',
+      scope: {      },
+
+      transclude: true
+
+
+    };
+
+
+  }
+
+
+}());
