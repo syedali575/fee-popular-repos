2bff4734ade5030abd87195c37ce16006d73f956
Syed Ali <sali3@cox.net>
2016-12-12 14:37:22 -0500

    Added a filter file

---
+++ app/gh.filter.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular.module('gh')
+    .filter('bypopularity', ByPopularity);
+
+    function ByPopularity(){
+
+      return function byPop(input) {
+
+        
+      };
+
+    }
+
+
+
+
+}());
